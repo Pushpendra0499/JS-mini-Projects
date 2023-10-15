4286634efb7e4c3c4efe1c5a3a687eb0ccfa433b
Mukku <mukkuraj0499@gmail.com>
2023-10-15 22:27:14 +0530

    update code

--- 01-Background Color Switcher/index.js
+++ 01-Background Color Switcher/index.js
@@ -1,14 +1,17 @@
+//timming ==>  2:00:00
 
 const buttons = document.querySelectorAll('.button')
 const body =document.querySelector("body");
+// const nav =document.querySelector("nav");
 
 buttons.forEach( function (button){
   console.log(button);
   button.addEventListener('click',function(e){
-    console.log(e);
-    console.log(e.target);
+    // console.log(e);
+    // console.log(e.target);
     if(e.target.id === 'grey'){
       body.style.backgroundColor =e.target.id
+      
     }
     if(e.target.id === 'white'){
       body.style.backgroundColor =e.target.id
@@ -17,6 +20,9 @@ buttons.forEach( function (button){
       body.style.backgroundColor =e.target.id
     }
     if(e.target.id === 'yellow'){
+      body.style.backgroundColor =e.target.id
+    }
+    if(e.target.id === 'purple'){
       body.style.backgroundColor =e.target.id
     }
   })
