6e8f38320a48c1f8ac981ff40942bb625235a0b3
Mukku <mukkuraj0499@gmail.com>
2023-10-18 18:35:20 +0530

    update code

--- 06-unLimited/script.js
+++ 06-unLimited/script.js
@@ -1,15 +1,31 @@
-// ganerate a random color
 
-const randomColor =function(){
-    const hex ="012345678ABCDEF";
-    let color ='0';
-    for (let i = 0;i < 6;i++) {
-        color +=hex[Math.floor(Math.random() * 16)];
-    }
-    return color
+const hex = "0123456789ABCDEF";
 
+const randomColor = function() {
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
 };
-//console.log(randomColor());
-    
 
-/// last Timming 5:00:00 API request and v8 engine
+let intervalId;
+const startChangingColor = function() {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+};
+
+const stopChangingColor = function() {
+  clearInterval(intervalId);
+  intervalId = null; // Reset intervalId to null
+};
+
+document.querySelector("#start").addEventListener('click', startChangingColor);
+document.querySelector("#stop").addEventListener('click', stopChangingColor);
+
+function changeBgColor() {
+  document.body.style.backgroundColor = randomColor();
+}
+
+
