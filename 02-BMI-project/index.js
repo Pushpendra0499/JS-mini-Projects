4dc9d98cbf3536b9ccff462ec007e911104f5b6f
Mukku <mukkuraj0499@gmail.com>
2023-10-23 10:09:32 +0530

    Update code

--- 02-BMI-project/index.js
+++ 02-BMI-project/index.js
@@ -1,27 +1,20 @@
-// **** 2:13:00
 const form = document.querySelector('form');
-// this usecase will give you empty
-// const height = parseInt(document.querySelector('#height').value)
+const heightInput = document.querySelector('#height');
+const weightInput = document.querySelector('#weight');
+const results = document.querySelector('#results');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  const height = parseInt(document.querySelector('#height').value);
-  const weight = parseInt(document.querySelector('#weight').value);
-  const results = document.querySelector('#results');
+  const height = parseInt(heightInput.value);
+  const weight = parseInt(weightInput.value);
 
-  if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please give a valid height ${height}`;
-   }
-  else if (weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please give a valid weight ${weight}`;
-  }
-  else {
+  if (isNaN(height) || height <= 0) {
+    results.innerHTML = `Please provide a valid height: ${height}`;
+  } else if (isNaN(weight) || weight <= 0) {
+    results.innerHTML = `Please provide a valid weight: ${weight}`;
+  } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //show the result
     results.innerHTML = `<span>${bmi}</span>`;
   }
-
 });
-
-
