b936bb2331edf7277527c03d4e987f8230535fcb
Mukku <mukkuraj0499@gmail.com>
2023-10-25 08:28:22 +0530

    update code

--- 03-digitalclock/index.js
+++ 03-digitalclock/index.js
@@ -1,11 +1,11 @@
-// ******2:31:00
 
 // document.getElementById('clock')
 const clock = document.querySelector('#clock')
 
 setInterval(function () {
     let date =new Date();
-    // console.log(date.toLocaleTimeString());
+    console.log(date.toLocaleTimeString());
     clock.innerHTML = date.toLocaleTimeString();
 },1000)
 
+    
